76187f6521c294a55e2ba25a16eaefbd69e4f7ba
fstawicki <stawickifilip1@gmail.com>
2022-02-03 00:11:39 +0100

    added order, button in navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -15,7 +15,11 @@ const Navbar = (props) => {
                     <li className={styles.listItem}><a href="#">O Nas</a></li>
                     <li className={styles.listItem}><a href="#">Galeria</a></li>
                     <li className={styles.listItem}><a href="#">Kontakt</a></li>
+                    <li className={styles.listItemBtn}>
+                    <button className={styles.btn}><a href="https://forms.gle/3B1aczqwx2XDw6mw7">ZŁÓŻ ZAMÓWIENIE</a></button>
+                    </li>
                 </ul>
+                
             </div>
         </nav>
     )
--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -5,7 +5,7 @@ import styles from './Order.module.css';
 const Order = (props) => {
     return(
         <div className={styles.container}>
-            <button className={styles.container}>
+            <button className={styles.btn}>
                 <a href="https://forms.gle/3B1aczqwx2XDw6mw7">ZŁÓŻ ZAMÓWIENIE</a> 
             </button>
         </div>
