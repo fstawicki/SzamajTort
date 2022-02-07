6d0875ea33e919a06c6702ce719d106e1d225884
fstawicki <stawickifilip1@gmail.com>
2022-02-08 00:41:10 +0100

    added hamburger menu. To do: styling of heropage btn ; navbar logic

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from './Navbar.module.css';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { AiOutlineClose } from 'react-icons/ai';
 
 
 const Navbar = (props) => {
+
+    const [toggleMenu, setToggleMenu] = useState(false);
+
 
     return(
         <nav className={styles.navbar}>
@@ -12,14 +17,39 @@ const Navbar = (props) => {
             <div className={styles.right}>
                 <ul className={styles.list}>
                     <li className={styles.listItem}><a href="#">Aktualności</a></li>
-                    <li className={styles.listItem}><a href="">O Nas</a></li>
-                    <li className={styles.listItem}><a href="#gallery">Galeria</a></li>
-                    <li className={styles.listItem}><a href="#">Kontakt</a></li>
+                    <li className={styles.listItem}><a href="About">O Nas</a></li>
+                    <li className={styles.listItem}><a href="#Galery">Galeria</a></li>
+                    <li className={styles.listItem}><a href="#Contact">Kontakt</a></li>
                     <li className={styles.listItemBtn}>
                     <button className={styles.btn}><a href="https://forms.gle/3B1aczqwx2XDw6mw7">ZŁÓŻ ZAMÓWIENIE</a></button>
                     </li>
                 </ul>
                 
+            </div>
+    
+            <div className={styles.navbarSmallscreen}>
+                <GiHamburgerMenu className={styles.hamburgerIcon} onClick={() => {  
+                    setToggleMenu(true);
+                }} />
+                {toggleMenu && (
+                <div className={styles.smallscreenOverlay}>
+                    <div className={styles.overlayCloseDiv}>
+
+                <AiOutlineClose className={styles.overlayClose} onClick={() => {
+                    setToggleMenu(false);
+                }} />
+                    </div>
+                <ul className={styles.listSmall}>
+                    <li className={styles.listItemSmall}><a href="#">Aktualności</a></li>
+                    <li className={styles.listItemSmall}><a href="#About">O Nas</a></li>
+                    <li className={styles.listItemSmall}><a href="#Galery">Galeria</a></li>
+                    <li className={styles.listItemSmall}><a href="#Contact">Kontakt</a></li>
+                    <li className={styles.listItemSmallLast}>
+                    <a href="https://forms.gle/3B1aczqwx2XDw6mw7">ZŁÓŻ ZAMÓWIENIE</a>
+                    </li>
+                </ul>
+                </div>
+            )}
             </div>
         </nav>
     )
