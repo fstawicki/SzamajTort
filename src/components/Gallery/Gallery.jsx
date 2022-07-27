5b06ce2e517e67ddefd5ebecdbf3c8a56dc5c93b
fstawicki <stawickifilip1@gmail.com>
2022-07-27 21:54:27 +0200

    working on carrousel

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -4,15 +4,26 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
 
 import styles from './Gallery.module.css';
-import { images } from "../../constants";
 
 import { BsArrowLeftShort, BsArrowRightShort } from 'react-icons/bs';
+
 
 
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
+
+
+import gallery01 from '../../images/gallery01.jpg';
+import gallery02 from '../../images/gallery02.jpg';
+import gallery03 from '../../images/gallery03.jpg';
+import gallery04 from '../../images/gallery04.jpg';
+import gallery05 from '../../images/gallery05.jpg';
+import gallery06 from '../../images/gallery06.jpg';
+import gallery07 from '../../images/gallery07.jpg';
+import gallery08 from '../../images/gallery08.jpg';
+
 
 
 const Galery = (props) => {
@@ -30,7 +41,9 @@ const Galery = (props) => {
     //     }
     // }
 
-    const galleryImages = [images.gallery03, images.gallery02, images.gallery01, images.gallery04, images.gallery05, images.gallery06, images.gallery07, images.gallery08];
+    const galleryImages = [
+        gallery01, gallery02, gallery03, gallery04, gallery05, gallery06, gallery07, gallery08
+    ];
 
     return(
         <div id="Gallery" className={styles.gallery}>
@@ -40,35 +53,18 @@ const Galery = (props) => {
             </div>
             <div className={styles.galleryright}>
             <Swiper
-      // install Swiper modules
-      modules={[Navigation, Pagination, Scrollbar, A11y]}
-      spaceBetween={50}
-      slidesPerView={3}
-      navigation
-      pagination={{ clickable: true }}
-      scrollbar={{ draggable: true }}
-      onSwiper={(swiper) => console.log(swiper)}
-      onSlideChange={() => console.log('slide change')}
-    >
-      <SwiperSlide>Slide 1</SwiperSlide>
-      <SwiperSlide>Slide 2</SwiperSlide>
-      <SwiperSlide>Slide 3</SwiperSlide>
-      <SwiperSlide>Slide 4</SwiperSlide>
-      ...
-    </Swiper>
-
-
-                {/* <div className={styles.galleryright_container} ref={scrollRef}>
-                    {galleryImages.map((image, index) => (
-                        <div className={styles.galleryright_card} key={`galery-image-${index + 1}`}>
-                            <img src={image} alt="galery" />
-                        </div>
-                    ))}
-                </div>
-                <div className={styles.galleryright_arrows}>
-                    <BsArrowLeftShort className={styles.arrowIcon} onClick={() => scroll('left')} />
-                    <BsArrowRightShort className={styles.arrowIcon} onClick={() => scroll('right')} />
-                </div> */}
+                modules={[Navigation, Pagination, Scrollbar, A11y]}
+                spaceBetween={50}
+                slidesPerView={3}
+                navigation
+                pagination={{ clickable: true }}
+                scrollbar={{ draggable: true }}
+            >
+                {galleryImages.map((image) => {
+                        return <SwiperSlide style={{ backgroundImage: `url(${image})` }}></SwiperSlide>;
+                    })
+                }
+            </Swiper>
             </div>
         </div>
 
